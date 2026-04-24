bffaaa59fc10937bf1d53ec0752c4e2d4b65c88f
BASSKILL <1436656902@qq.com>
2026-04-24 21:24:07 +0800

    补充证书，更新游戏到1.2

---
+++ web-demo/ServiceWorker.js
@@ -0,0 +1,33 @@
+const cacheName = ".LINE-我和我的像素交大-1.1";
+const contentToCache = [
+    "Build/web-demo.loader.js",
+    "Build/web-demo.framework.js.br",
+    "Build/web-demo.data.br",
+    "Build/web-demo.wasm.br",
+    "TemplateData/style.css"
+
+];
+
+self.addEventListener('install', function (e) {
+    console.log('[Service Worker] Install');
+    
+    e.waitUntil((async function () {
+      const cache = await caches.open(cacheName);
+      console.log('[Service Worker] Caching all: app shell and content');
+      await cache.addAll(contentToCache);
+    })());
+});
+
+self.addEventListener('fetch', function (e) {
+    e.respondWith((async function () {
+      let response = await caches.match(e.request);
+      console.log(`[Service Worker] Fetching resource: ${e.request.url}`);
+      if (response) { return response; }
+
+      response = await fetch(e.request);
+      const cache = await caches.open(cacheName);
+      console.log(`[Service Worker] Caching new resource: ${e.request.url}`);
+      cache.put(e.request, response.clone());
+      return response;
+    })());
+});
