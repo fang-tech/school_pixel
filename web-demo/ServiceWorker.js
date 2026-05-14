124f4cb96993204cbaad1a3a090304fb4072dcd7
BASSKILL <1436656902@qq.com>
2026-05-14 18:31:22 +0800

    游戏版本v1.7.1

--- web-demo/ServiceWorker.js
+++ web-demo/ServiceWorker.js
@@ -1,4 +1,4 @@
-const cacheName = ".LINE-我和我的像素交大-1.6.5";
+const cacheName = ".LINE-我和我的像素交大-1.7.1";
 const contentToCache = [
     "Build/web-demo.loader.js",
     "Build/web-demo.framework.js.br",
